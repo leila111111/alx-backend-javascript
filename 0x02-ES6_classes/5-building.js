54bf72e4c697f17d446cb1e7a5a7617efd694747
leila111111 <leila_merzak@outlook.fr>
2024-01-04 18:24:31 +0000

    task5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,11 +1,11 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building) {
-	    if (this.evacuationWarningMessage === undefined) {
+	    if (typeof this.evacuationWarningMessage !== 'function') {
       	      throw new Error('Class extending Building must override evacuationWarningMessage');
             }
-    this.sqft = sqft;
     }
+    this.sqft = sqft;
   }
 
   get sqft() {
