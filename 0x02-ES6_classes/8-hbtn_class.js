552925fae94925ec91195bf4ef8cb5e18e5cd9fb
leila111111 <leila_merzak@outlook.fr>
2024-01-04 18:49:54 +0000

    tasks

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,32 @@
-#!/usr/bin/node
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](val) {
+    if (val === 'number') {
+      return this.size;
+    }
+    if (val === 'string') {
+      return this.location;
+    }
+    return this;
+  }
+}
