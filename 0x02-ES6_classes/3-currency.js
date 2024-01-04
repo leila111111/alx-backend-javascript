76600937d6b80a0d2975fdd5dd1491791aebb04b
leila111111 <leila_merzak@outlook.fr>
2024-01-04 17:52:54 +0000

    task3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -7,12 +7,15 @@ export default class Currency {
   get name() {
     return this._name;
   }
+
   set code(value) {
     this._code = value;
   }
+
   set name(value) {
     this._name = value;
   }
+
   get code() {
     return this._code;
   }
