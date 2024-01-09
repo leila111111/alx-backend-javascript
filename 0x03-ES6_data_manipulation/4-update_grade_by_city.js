128baef99d32c077f7b486834abd67f12d45aca6
leila111111 <leila_merzak@outlook.fr>
2024-01-09 22:55:49 +0000

    task 0 - 3

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function updateStudentGradeByCity(student, city, newGrades) {
+  const grades = newGrades.reduce((arr, obj) => ({
+    ...arr,
+    [obj.studentId]: obj.grade,
+  }), {});
+  const stdt = student.filter((element) => element.location === city).map((elmnt) => ({
+    ...elmnt,
+    grade: grades[elmnt.id] || 'N/A',
+  }));
+  return stdt;
+}
