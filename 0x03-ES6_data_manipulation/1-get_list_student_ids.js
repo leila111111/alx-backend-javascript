6c8375f768d4ae986c90fa38820100c89b92b681
leila111111 <leila_merzak@outlook.fr>
2024-01-09 22:41:19 +0000

    task 0 - 3

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(student) {
-  if (student instanceof Array){
+  if (student instanceof Array) {
     return student.map((element) => element.id);
   }
   return [];
