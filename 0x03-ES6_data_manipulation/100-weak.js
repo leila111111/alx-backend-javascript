8e69e6f900e3049350727efbb04611ddb0c779d9
leila111111 <leila_merzak@outlook.fr>
2024-01-09 23:15:39 +0000

    tasks

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+export const weakMap = new WeakMap();
+export function queryAPI(endpoint) {
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function cleanSet(set, startString) {
+  if (!startString || startString.length === 0) {
+    return '';
+  }
+  const setToArray = Array.from(set)
+    .filter((element) => typeof element === 'string' && element.startsWith(startString))
+    .map((element) => ele.slice(startString.length));
+
+  return setToArray.join('-');
+}
