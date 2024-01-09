1de56c4b8da3bf9434ccc25d2af408a3c9f70c67
leila111111 <leila_merzak@outlook.fr>
2024-01-09 23:41:30 +0000

    tasks

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,6 +1,6 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
-  element = weakMap.get(endpoint) || 0;
+  let element = weakMap.get(endpoint) || 0;
   element++;
   if (element >= 5) {
     throw new Error('Endpoint load is high');
