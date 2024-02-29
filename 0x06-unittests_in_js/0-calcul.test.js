4869fc51d5d11b9d59a0fd73e5bb540d8fef772b
leila111111 <leila_merzak@outlook.fr>
2024-02-29 23:47:51 +0000

    task1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,11 +2,20 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('testing float numbers', () => {
-    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
-    assert.strictEqual(calculateNumber(1.0, 3.2), 3);
-    assert.strictEqual(calculateNumber(3.2, 1.0), 3);
-    assert.strictEqual(calculateNumber(1.2, 3.2), 3);
-    assert.strictEqual(calculateNumber(1.6, 2.2), 5);
-  });
+  it('testing floats ', () => {
+    assert.strictEqual(calculateNumber(1.0, 3.0), 4);
+
+    assert.strictEqual(calculateNumber(1.0, 1.4), 2);
+
+    assert.strictEqual(calculateNumber(1.4, 1.4), 2);
+
+    assert.strictEqual(calculateNumber(1.4, 1.0), 2);
+
+    assert.strictEqual(calculateNumber(1.5, 2), 4);
+
+    assert.strictEqual(calculateNumber(2.7, 2.6), 6);
+
+    assert.strictEqual(calculateNumber(2.7, 2.0), 5);
+
+    assert.strictEqual(calculateNumber(2.499999, 2.499999), 4);
 });
