3ef84ae259f4b9c7b149a4dede7398dbe47eb6ad
leila111111 <leila_merzak@outlook.fr>
2024-03-01 01:42:16 +0000

    task10

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,1 +1,52 @@
-#!/usr/bin/node
+const request = require('request');
+const { expect } = require('chai');
+
+describe('api test', () => {
+
+  it('correct response', (done) => {
+    request('http://localhost:7865', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('status code 200 and correct response', (done) => {
+    request('http://localhost:7865/cart/16', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 16');
+      done();
+    });
+  });
+
+  it('status 404', (done) => {
+    request('http://localhost:7865/cart/quiqiw', (err, res) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('status 404 negative', (done) => {
+    request('http://localhost:7865/cart/-16', (err, res) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('correct response', (done) => {
+    request('http://localhost:7865/login', {json: {userName: 'samanta'}}, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome samanta');
+      done();
+    });
+  });
+
+  it('correct response available payments', (done) => {
+    request('http://localhost:7865/available_payments', (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(JSON.parse(body))
+        .to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false}});
+      done();
+    });
+  });
+});
