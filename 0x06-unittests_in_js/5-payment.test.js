917e5b16249fbbe5e0a894a7d37d0210bedc56f2
leila111111 <leila_merzak@outlook.fr>
2024-03-01 01:06:03 +0000

    task4

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToAPI = require('./5-payment');
+
+describe('sendpaymentReqtoApi', () => {
+  let calcul;
+
+  beforeEach(() => {
+    calcul = sinon.spy(console, 'log');
+  });
+  afterEach(() => {
+    calcul.restore();
+  });
+
+  it('The Total is 120', () => {
+    sendPaymentRequestToAPI(100, 20);
+    expect(calcul.calledOnce).to.be.true;
+    expect(calcul.calledWithExactly('The total is: 120')).to.be.true;
+  });
+
+  it('The Total is 20', () => {
+    sendPaymentRequestToAPI(10, 10);
+    expect(calcul.calledOnce).to.be.true;
+    expect(calcul.calledWithExactly('The total is: 20')).to.be.true;
+  }); 
+});
