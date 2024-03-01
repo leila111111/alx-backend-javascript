fcacd54615ba20aff3ea2aa2511978d86a64895c
leila111111 <leila_merzak@outlook.fr>
2024-03-01 23:51:59 +0000

    task2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,29 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+function countStudents(datapath) {
+    if (!fs.existsSync(dataPath)) {
+     throw new Error('Cannot load the database');
+    }
+    else {
+    const names = {};
+    const fields = {};
+    const fileread = fs.readFileSync(path, 'utf8');
+    const filelines = fileread.toString().trim().split('\n');
+    console.log(`Number of students: ${filelines.length - 1}`);
+
+    filelines.slice(1).forEach((str) => {
+      const rec = str.split(',');
+      const field = rec.pop();
+      fields[field] = Object.prototype.hasOwnProperty.call(fields, field) ? fields[field] + 1 : 1;
+      names[field] = Object.prototype.hasOwnProperty.call(names, field)
+        ? [...names[field], rec[0]]
+        : [rec[0]];
+    });
+    for (const [key, val] of Object.entries(fields)) {
+      const str = names[key].join(', ');
+      console.log(`Number of students in ${key}: ${val}. List: ${str}`);
+    }
+  }
+}
+
+module.exports = countStudents;
