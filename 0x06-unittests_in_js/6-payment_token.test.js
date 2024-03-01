5f4f2cae61d3bfb1615b501e68b69b689650c56f
leila111111 <leila_merzak@outlook.fr>
2024-03-01 01:09:20 +0000

    task6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('get payment token', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((result) => {
+        expect(result).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      });
+  });
+});
