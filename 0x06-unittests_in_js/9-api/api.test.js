f8a77ac2c2508ae9ad68885128f07275745e8a17
leila111111 <leila_merzak@outlook.fr>
2024-03-01 01:22:13 +0000

    task9

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,1 +1,28 @@
-#!/usr/bin/node
+const { expect } = require('chai');
+const request = require('request');
+
+describe('test api', () => {
+  it('correct response', (done) => {
+    request('http://localhost:7865', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+  
+
+  it('status code 200 and correct response', (done) => {
+    request('http://localhost:7865/cart/16', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 16');
+      done();
+    });
+  });
+
+  it('status 404', (done) => {
+    request('http://localhost:7865/cart/quiqiw', (err, res) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
