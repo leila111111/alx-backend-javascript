dbb2d31b494f56d982369aa9fc652adec2069807
leila111111 <leila_merzak@outlook.fr>
2024-03-01 00:52:45 +0000

    task3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+const sendPaymentRequestToApi = require('./3-payment');
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('send payment', () => {
+    const calcul = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+    expect(calcul.calledWith('SUM', 100, 20)).to.be.true;
+    expect(calcul.callCount).to.be.equal(1);
+    calcul.restore();
+  });
+});
