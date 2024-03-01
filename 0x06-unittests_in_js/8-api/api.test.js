bf0278bf1756cc4e71249e15ffbc86c1bf4719cb
leila111111 <leila_merzak@outlook.fr>
2024-03-01 01:16:28 +0000

    task8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const { expect } = require('chai');
+const request = require('request');
+
+describe('api test', () => {
+  it('correct response', (done) => {
+    request('http://localhost:7865', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
