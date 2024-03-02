890b40778f0b26c192a65d2c60b8e27bb453df63
leila111111 <leila_merzak@outlook.fr>
2024-03-02 00:40:18 +0000

    task5 6 7

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,1 +1,32 @@
-#!/usr/bin/node
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+
+const port = 1245;
+const path = process.argv[2];
+
+const app = http.createServer(async (req, res) => {
+  const check = url.parse(req.url, true);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (check.pathname === '/') {
+    res.end('Hello Holberton School!');
+    return;
+  }
+
+  if (check.pathname === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const data = await countStudents(path);
+      res.end(data.join('\n'));
+    } catch (error) {
+      res.end(error.message);
+    }
+    return;
+  }
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end('Not Found');
+});
+
+app.listen(port);
+module.exports = app;
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,1 +1,21 @@
-#!/usr/bin/node
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const database = process.argv[2];
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  const response = 'This is the list of our students\n';
+  countStudents(database).then((data) => {
+    res.send(`${response}${data.join('\n')}`);
+  }).catch((error) => {
+    res.send(`${response}${error.message}`);
+  });
+});
+app.listen(port);
+module.exports = app;
